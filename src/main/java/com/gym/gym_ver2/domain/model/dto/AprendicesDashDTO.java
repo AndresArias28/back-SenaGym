55df789a4c307869582a89dff0ced47704c336b3
andres naranjo <santo2828@gmail.com>
2025-10-08 13:30:51 -0500

    agregar ipPersona en respuesta de aprendicesDasboard

--- src/main/java/com/gym/gym_ver2/domain/model/dto/AprendicesDashDTO.java
+++ src/main/java/com/gym/gym_ver2/domain/model/dto/AprendicesDashDTO.java
@@ -6,6 +6,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AprendicesDashDTO {
+    private Integer id;
     private String nombre;
     private Integer ficha;
     private String nivel;
