64c591797530bba504a5ddc9c358ed6d25931291
andres naranjo <santo2828@gmail.com>
2025-10-10 14:01:22 -0500

    corregir asignacionRutinaRepo

--- src/main/java/com/gym/gym_ver2/infraestructure/persistence/repository/AsignacionRutinaRepository.java
+++ src/main/java/com/gym/gym_ver2/infraestructure/persistence/repository/AsignacionRutinaRepository.java
@@ -12,7 +12,7 @@ public interface AsignacionRutinaRepository extends JpaRepository<AsignacionRuti
     List<AsignacionRutina> findByAprendiz_IdPersona(Integer idPersona);
 
     @Query("SELECT new com.gym.gym_ver2.domain.model.dto.AsignacionesResponse(" +
-            "CONCAT(a.nombres, ' ', a.apellidos), a.idPersona, a.ficha, a.   " +
+            "CONCAT(a.nombres, ' ', a.apellidos), a.idPersona, a.ficha, " +
             "ar.fechaAsignacion, a.nivelFisico, ar.observaciones, ar.diaAsignado, ar.rutina.nombre) " +
             "FROM AsignacionRutina ar " +
             "JOIN ar.aprendiz a " +
